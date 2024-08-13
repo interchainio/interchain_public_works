3f451ba238ac4407ff72d7011f7476f058e40ecd
JPB <jonathanpberger@gmail.com>
2024-08-13 10:33:42 -0400

    WIP

--- copy_epics.rb
+++ copy_epics.rb
@@ -1,21 +1,87 @@
 require "json"
 
-# cat ./mocks/backlogs/interchaintest.json| ruby ./copy_epics.rb
+# TODO
+# 1. swich from stubs to live data (do we actually need to do this? Could just get fresh stubs)
+# 2. HMW read the **status** from the enemy story and map it to our status?
 
-project_backlog = JSON.parse(STDIN.read)
+# Directory containing the JSON files
+backlog_stubs = "./mocks/backlogs"
 
-project_id = 'PVTI_lADOAsuzxc4AkKP5zgQ9Oxg'
-field_id = 'PVTSSF_lADOAsuzxc4AkKP5zgcaQoo'
-option_id = '282f80fd'
-owner_of_overarching_project = 'strangelove-ventures'
-id_number_of_overarching_project = 34
+# Clear the run_me.sh file if it exists
+run_me_file = "run_me.sh"
+File.open(run_me_file, "w") {}
 
-project_backlog["items"].each do |issues|
-  if issues.key?("story Type")
-    if issues["story Type"].include?("Epic") and issues["content"]["type"] == "Issue"
-      issue_url = issues["content"]["url"]
-      puts "gh project item-add #{id_number_of_overarching_project} --url #{issue_url} --owner #{owner_of_overarching_project}"
-      puts "gh project item-edit --id #{issues["id"]} --project-id #{project_id} --field-id #{field_id} --single-select-option-id string #{option_id}"
+# Project-specific details
+overarching_project_owner_of_overarching_project = 'strangelove-ventures'
+overarching_project_number                       = 34 # design project, for testing
+overarching_project_id                           = 'PVTI_lADOAsuzxc4AkKP5zgQ9Oxg'
+ipw_story_type_field_id                          = 'PVTSSF_lADOAsuzxc4AkKP5zgcaQoo'
+ipw_option_id_for_epic                           = '282f80fd'
+ipw_story_status_field_id                        = ""
+ipw_option_id_for_story_status                   = ""
+
+
+# story_status for ipw
+# {
+#   "id": "PVTSSF_lADOAsuzxc4AkKP5zgcaQn8",
+#   "name": "Status",
+#   "options": [
+#     {
+#       "id": "d958ab90",
+#       "name": "☃️ Icebox"
+#     },
+#     {
+#       "id": "f75ad846",
+#       "name": "📋 Backlog"
+#     },
+#     {
+#       "id": "47fc9ee4",
+#       "name": "🤸‍♂️ In Progress"
+#     },
+#     {
+#       "id": "2ab45224",
+#       "name": "👀 Waiting / In review"
+#     },
+#     {
+#       "id": "98236657",
+#       "name": "🥳 Done"
+#     }
+#   ],
+#   "type": "ProjectV2SingleSelectField"
+# },
+
+#       |id       | name|
+#       |---------|-----|
+#       |d958ab90 |☃️ Icebox|
+#       |f75ad846 |📋 Backlog|
+#       |47fc9ee4 |🤸‍♂️ In Progress|
+#       |2ab45224 |👀 Waiting / In review|
+#       |98236657 |🥳 Done|
+
+# Iterate over each JSON file in the backlog_stubs
+Dir.glob("#{backlog_stubs}/*.json").each do |file_path|
+  project_backlog = JSON.parse(File.read(file_path))
+
+  File.open(run_me_file, "a") do |file|
+    project_backlog["items"].each do |issues|
+      if issues.key?("story Type")
+        if issues["story Type"].include?("Epic") and issues["content"]["type"] == "Issue"
+          issue_url = issues["content"]["url"]
+          issue_story_status = issues["status"]
+          file.puts "### Item Add: #{issue_url}"
+          file.puts "gh project item-add #{overarching_project_number} --url #{issue_url} --owner #{overarching_project_owner_of_overarching_project}"
+          file.puts "\n"
+          file.puts "#### Item Edit: #{issues["id"]}"
+          file.puts "##### Item Edit: set story type"
+          file.puts "gh project item-edit --id #{issues["id"]} --project-id #{overarching_project_id} --field-id #{ipw_story_type_field_id} --single-select-option-id string #{ipw_option_id_for_epic}"
+          file.puts "###### Item Edit: set story status #{issue_story_status}"
+          file.puts "gh project item-edit --id #{issues["id"]} --project-id #{overarching_project_id} --field-id #{ipw_story_status_field_id} --single-select-option-id string #{ipw_option_id_for_story_status}"
+          file.puts "\n # ---\n\n"
+        end
+      end
     end
   end
 end
+
+# Make the run_me.sh file executable
+system("chmod +x #{run_me_file}")
---
+++ run_me.sh
@@ -0,0 +1,232 @@
+### Item Add: https://github.com/cosmos/cosmos-sdk/issues/13085
+gh project item-add 34 --url https://github.com/cosmos/cosmos-sdk/issues/13085 --owner strangelove-ventures
+
+###### Item Edit: PVTI_lADOAAN9684ABqEwzgCSefg
+gh project item-edit --id PVTI_lADOAAN9684ABqEwzgCSefg --project-id PVTI_lADOAsuzxc4AkKP5zgQ9Oxg --field-id PVTSSF_lADOAsuzxc4AkKP5zgcaQoo --single-select-option-id string 282f80fd
+
+ # ---
+
+### Item Add: https://github.com/cosmos/cosmos-sdk/issues/11899
+gh project item-add 34 --url https://github.com/cosmos/cosmos-sdk/issues/11899 --owner strangelove-ventures
+
+###### Item Edit: PVTI_lADOAAN9684ABqEwzgBGdRU
+gh project item-edit --id PVTI_lADOAAN9684ABqEwzgBGdRU --project-id PVTI_lADOAsuzxc4AkKP5zgQ9Oxg --field-id PVTSSF_lADOAsuzxc4AkKP5zgcaQoo --single-select-option-id string 282f80fd
+
+ # ---
+
+### Item Add: https://github.com/cosmos/cosmos-sdk/issues/9958
+gh project item-add 34 --url https://github.com/cosmos/cosmos-sdk/issues/9958 --owner strangelove-ventures
+
+###### Item Edit: PVTI_lADOAAN9684ABqEwzgBGIOU
+gh project item-edit --id PVTI_lADOAAN9684ABqEwzgBGIOU --project-id PVTI_lADOAsuzxc4AkKP5zgQ9Oxg --field-id PVTSSF_lADOAsuzxc4AkKP5zgcaQoo --single-select-option-id string 282f80fd
+
+ # ---
+
+### Item Add: https://github.com/cosmos/cosmos-sdk/issues/13039
+gh project item-add 34 --url https://github.com/cosmos/cosmos-sdk/issues/13039 --owner strangelove-ventures
+
+###### Item Edit: PVTI_lADOAAN9684ABqEwzgCSehg
+gh project item-edit --id PVTI_lADOAAN9684ABqEwzgCSehg --project-id PVTI_lADOAsuzxc4AkKP5zgQ9Oxg --field-id PVTSSF_lADOAsuzxc4AkKP5zgcaQoo --single-select-option-id string 282f80fd
+
+ # ---
+
+### Item Add: https://github.com/cosmos/cosmos-sdk/issues/17045
+gh project item-add 34 --url https://github.com/cosmos/cosmos-sdk/issues/17045 --owner strangelove-ventures
+
+###### Item Edit: PVTI_lADOAAN9684ABqEwzgIAN7M
+gh project item-edit --id PVTI_lADOAAN9684ABqEwzgIAN7M --project-id PVTI_lADOAsuzxc4AkKP5zgQ9Oxg --field-id PVTSSF_lADOAsuzxc4AkKP5zgcaQoo --single-select-option-id string 282f80fd
+
+ # ---
+
+### Item Add: https://github.com/cosmos/cosmos-sdk/issues/17781
+gh project item-add 34 --url https://github.com/cosmos/cosmos-sdk/issues/17781 --owner strangelove-ventures
+
+###### Item Edit: PVTI_lADOAAN9684ABqEwzgKmXrY
+gh project item-edit --id PVTI_lADOAAN9684ABqEwzgKmXrY --project-id PVTI_lADOAsuzxc4AkKP5zgQ9Oxg --field-id PVTSSF_lADOAsuzxc4AkKP5zgcaQoo --single-select-option-id string 282f80fd
+
+ # ---
+
+### Item Add: https://github.com/cosmos/cosmos-sdk/issues/17786
+gh project item-add 34 --url https://github.com/cosmos/cosmos-sdk/issues/17786 --owner strangelove-ventures
+
+###### Item Edit: PVTI_lADOAAN9684ABqEwzgKoqUw
+gh project item-edit --id PVTI_lADOAAN9684ABqEwzgKoqUw --project-id PVTI_lADOAsuzxc4AkKP5zgQ9Oxg --field-id PVTSSF_lADOAsuzxc4AkKP5zgcaQoo --single-select-option-id string 282f80fd
+
+ # ---
+
+### Item Add: https://github.com/cosmos/cosmos-sdk/issues/18141
+gh project item-add 34 --url https://github.com/cosmos/cosmos-sdk/issues/18141 --owner strangelove-ventures
+
+###### Item Edit: PVTI_lADOAAN9684ABqEwzgKorEE
+gh project item-edit --id PVTI_lADOAAN9684ABqEwzgKorEE --project-id PVTI_lADOAsuzxc4AkKP5zgQ9Oxg --field-id PVTSSF_lADOAsuzxc4AkKP5zgcaQoo --single-select-option-id string 282f80fd
+
+ # ---
+
+### Item Add: https://github.com/cosmos/cosmos-sdk/issues/18462
+gh project item-add 34 --url https://github.com/cosmos/cosmos-sdk/issues/18462 --owner strangelove-ventures
+
+###### Item Edit: PVTI_lADOAAN9684ABqEwzgKo5nM
+gh project item-edit --id PVTI_lADOAAN9684ABqEwzgKo5nM --project-id PVTI_lADOAsuzxc4AkKP5zgQ9Oxg --field-id PVTSSF_lADOAsuzxc4AkKP5zgcaQoo --single-select-option-id string 282f80fd
+
+ # ---
+
+### Item Add: https://github.com/cosmos/cosmos-sdk/issues/18374
+gh project item-add 34 --url https://github.com/cosmos/cosmos-sdk/issues/18374 --owner strangelove-ventures
+
+###### Item Edit: PVTI_lADOAAN9684ABqEwzgKsAm4
+gh project item-edit --id PVTI_lADOAAN9684ABqEwzgKsAm4 --project-id PVTI_lADOAsuzxc4AkKP5zgQ9Oxg --field-id PVTSSF_lADOAsuzxc4AkKP5zgcaQoo --single-select-option-id string 282f80fd
+
+ # ---
+
+### Item Add: https://github.com/cosmos/cosmos-sdk-docs/issues/84
+gh project item-add 34 --url https://github.com/cosmos/cosmos-sdk-docs/issues/84 --owner strangelove-ventures
+
+###### Item Edit: PVTI_lADOAAN9684ABqEwzgKsbUk
+gh project item-edit --id PVTI_lADOAAN9684ABqEwzgKsbUk --project-id PVTI_lADOAsuzxc4AkKP5zgQ9Oxg --field-id PVTSSF_lADOAsuzxc4AkKP5zgcaQoo --single-select-option-id string 282f80fd
+
+ # ---
+
+### Item Add: https://github.com/cosmos/cosmos-sdk/issues/18446
+gh project item-add 34 --url https://github.com/cosmos/cosmos-sdk/issues/18446 --owner strangelove-ventures
+
+###### Item Edit: PVTI_lADOAAN9684ABqEwzgKt4UQ
+gh project item-edit --id PVTI_lADOAAN9684ABqEwzgKt4UQ --project-id PVTI_lADOAsuzxc4AkKP5zgQ9Oxg --field-id PVTSSF_lADOAsuzxc4AkKP5zgcaQoo --single-select-option-id string 282f80fd
+
+ # ---
+
+### Item Add: https://github.com/cosmos/cosmos-sdk/issues/17041
+gh project item-add 34 --url https://github.com/cosmos/cosmos-sdk/issues/17041 --owner strangelove-ventures
+
+###### Item Edit: PVTI_lADOAAN9684ABqEwzgKt4u4
+gh project item-edit --id PVTI_lADOAAN9684ABqEwzgKt4u4 --project-id PVTI_lADOAsuzxc4AkKP5zgQ9Oxg --field-id PVTSSF_lADOAsuzxc4AkKP5zgcaQoo --single-select-option-id string 282f80fd
+
+ # ---
+
+### Item Add: https://github.com/cosmos/cosmos-sdk/issues/18523
+gh project item-add 34 --url https://github.com/cosmos/cosmos-sdk/issues/18523 --owner strangelove-ventures
+
+###### Item Edit: PVTI_lADOAAN9684ABqEwzgKxMDw
+gh project item-edit --id PVTI_lADOAAN9684ABqEwzgKxMDw --project-id PVTI_lADOAsuzxc4AkKP5zgQ9Oxg --field-id PVTSSF_lADOAsuzxc4AkKP5zgcaQoo --single-select-option-id string 282f80fd
+
+ # ---
+
+### Item Add: https://github.com/cosmos/cosmos-sdk/issues/18580
+gh project item-add 34 --url https://github.com/cosmos/cosmos-sdk/issues/18580 --owner strangelove-ventures
+
+###### Item Edit: PVTI_lADOAAN9684ABqEwzgK8ucg
+gh project item-edit --id PVTI_lADOAAN9684ABqEwzgK8ucg --project-id PVTI_lADOAsuzxc4AkKP5zgQ9Oxg --field-id PVTSSF_lADOAsuzxc4AkKP5zgcaQoo --single-select-option-id string 282f80fd
+
+ # ---
+
+### Item Add: https://github.com/cosmos/cosmos-sdk/issues/18591
+gh project item-add 34 --url https://github.com/cosmos/cosmos-sdk/issues/18591 --owner strangelove-ventures
+
+###### Item Edit: PVTI_lADOAAN9684ABqEwzgK-b3M
+gh project item-edit --id PVTI_lADOAAN9684ABqEwzgK-b3M --project-id PVTI_lADOAsuzxc4AkKP5zgQ9Oxg --field-id PVTSSF_lADOAsuzxc4AkKP5zgcaQoo --single-select-option-id string 282f80fd
+
+ # ---
+
+### Item Add: https://github.com/cosmos/cosmos-sdk/issues/17579
+gh project item-add 34 --url https://github.com/cosmos/cosmos-sdk/issues/17579 --owner strangelove-ventures
+
+###### Item Edit: PVTI_lADOAAN9684ABqEwzgLcfu8
+gh project item-edit --id PVTI_lADOAAN9684ABqEwzgLcfu8 --project-id PVTI_lADOAsuzxc4AkKP5zgQ9Oxg --field-id PVTSSF_lADOAsuzxc4AkKP5zgcaQoo --single-select-option-id string 282f80fd
+
+ # ---
+
+### Item Add: https://github.com/cosmos/cosmos-sdk/issues/19906
+gh project item-add 34 --url https://github.com/cosmos/cosmos-sdk/issues/19906 --owner strangelove-ventures
+
+###### Item Edit: PVTI_lADOAAN9684ABqEwzgN3E_U
+gh project item-edit --id PVTI_lADOAAN9684ABqEwzgN3E_U --project-id PVTI_lADOAsuzxc4AkKP5zgQ9Oxg --field-id PVTSSF_lADOAsuzxc4AkKP5zgcaQoo --single-select-option-id string 282f80fd
+
+ # ---
+
+### Item Add: https://github.com/cosmos/cosmos-sdk/issues/20058
+gh project item-add 34 --url https://github.com/cosmos/cosmos-sdk/issues/20058 --owner strangelove-ventures
+
+###### Item Edit: PVTI_lADOAAN9684ABqEwzgOSi0Q
+gh project item-edit --id PVTI_lADOAAN9684ABqEwzgOSi0Q --project-id PVTI_lADOAsuzxc4AkKP5zgQ9Oxg --field-id PVTSSF_lADOAsuzxc4AkKP5zgcaQoo --single-select-option-id string 282f80fd
+
+ # ---
+
+### Item Add: https://github.com/cosmos/cosmos-sdk/issues/20067
+gh project item-add 34 --url https://github.com/cosmos/cosmos-sdk/issues/20067 --owner strangelove-ventures
+
+###### Item Edit: PVTI_lADOAAN9684ABqEwzgOT4_U
+gh project item-edit --id PVTI_lADOAAN9684ABqEwzgOT4_U --project-id PVTI_lADOAsuzxc4AkKP5zgQ9Oxg --field-id PVTSSF_lADOAsuzxc4AkKP5zgcaQoo --single-select-option-id string 282f80fd
+
+ # ---
+
+### Item Add: https://github.com/cosmos/cosmos-sdk/issues/20267
+gh project item-add 34 --url https://github.com/cosmos/cosmos-sdk/issues/20267 --owner strangelove-ventures
+
+###### Item Edit: PVTI_lADOAAN9684ABqEwzgOvCH0
+gh project item-edit --id PVTI_lADOAAN9684ABqEwzgOvCH0 --project-id PVTI_lADOAsuzxc4AkKP5zgQ9Oxg --field-id PVTSSF_lADOAsuzxc4AkKP5zgcaQoo --single-select-option-id string 282f80fd
+
+ # ---
+
+### Item Add: https://github.com/cosmos/cosmos-sdk/issues/20279
+gh project item-add 34 --url https://github.com/cosmos/cosmos-sdk/issues/20279 --owner strangelove-ventures
+
+###### Item Edit: PVTI_lADOAAN9684ABqEwzgOwIrQ
+gh project item-edit --id PVTI_lADOAAN9684ABqEwzgOwIrQ --project-id PVTI_lADOAsuzxc4AkKP5zgQ9Oxg --field-id PVTSSF_lADOAsuzxc4AkKP5zgcaQoo --single-select-option-id string 282f80fd
+
+ # ---
+
+### Item Add: https://github.com/cosmos/cosmos-sdk-docs/issues/139
+gh project item-add 34 --url https://github.com/cosmos/cosmos-sdk-docs/issues/139 --owner strangelove-ventures
+
+###### Item Edit: PVTI_lADOAAN9684ABqEwzgOxFsI
+gh project item-edit --id PVTI_lADOAAN9684ABqEwzgOxFsI --project-id PVTI_lADOAsuzxc4AkKP5zgQ9Oxg --field-id PVTSSF_lADOAsuzxc4AkKP5zgcaQoo --single-select-option-id string 282f80fd
+
+ # ---
+
+### Item Add: https://github.com/cosmos/cosmos-sdk/issues/20352
+gh project item-add 34 --url https://github.com/cosmos/cosmos-sdk/issues/20352 --owner strangelove-ventures
+
+###### Item Edit: PVTI_lADOAAN9684ABqEwzgO63Qo
+gh project item-edit --id PVTI_lADOAAN9684ABqEwzgO63Qo --project-id PVTI_lADOAsuzxc4AkKP5zgQ9Oxg --field-id PVTSSF_lADOAsuzxc4AkKP5zgcaQoo --single-select-option-id string 282f80fd
+
+ # ---
+
+### Item Add: https://github.com/cosmos/cosmos-sdk/issues/20439
+gh project item-add 34 --url https://github.com/cosmos/cosmos-sdk/issues/20439 --owner strangelove-ventures
+
+###### Item Edit: PVTI_lADOAAN9684ABqEwzgPPyxE
+gh project item-edit --id PVTI_lADOAAN9684ABqEwzgPPyxE --project-id PVTI_lADOAsuzxc4AkKP5zgQ9Oxg --field-id PVTSSF_lADOAsuzxc4AkKP5zgcaQoo --single-select-option-id string 282f80fd
+
+ # ---
+
+### Item Add: https://github.com/cosmos/cosmos-sdk/issues/14753
+gh project item-add 34 --url https://github.com/cosmos/cosmos-sdk/issues/14753 --owner strangelove-ventures
+
+###### Item Edit: PVTI_lADOAAN9684ABqEwzgPrtv0
+gh project item-edit --id PVTI_lADOAAN9684ABqEwzgPrtv0 --project-id PVTI_lADOAsuzxc4AkKP5zgQ9Oxg --field-id PVTSSF_lADOAsuzxc4AkKP5zgcaQoo --single-select-option-id string 282f80fd
+
+ # ---
+
+### Item Add: https://github.com/cosmos/cosmos-sdk/issues/21040
+gh project item-add 34 --url https://github.com/cosmos/cosmos-sdk/issues/21040 --owner strangelove-ventures
+
+###### Item Edit: PVTI_lADOAAN9684ABqEwzgRJ-Hc
+gh project item-edit --id PVTI_lADOAAN9684ABqEwzgRJ-Hc --project-id PVTI_lADOAsuzxc4AkKP5zgQ9Oxg --field-id PVTSSF_lADOAsuzxc4AkKP5zgcaQoo --single-select-option-id string 282f80fd
+
+ # ---
+
+### Item Add: https://github.com/strangelove-ventures/interchaintest/issues/882
+gh project item-add 34 --url https://github.com/strangelove-ventures/interchaintest/issues/882 --owner strangelove-ventures
+
+###### Item Edit: PVTI_lADOBTYxNs4AkKTLzgQlDgY
+gh project item-edit --id PVTI_lADOBTYxNs4AkKTLzgQlDgY --project-id PVTI_lADOAsuzxc4AkKP5zgQ9Oxg --field-id PVTSSF_lADOAsuzxc4AkKP5zgcaQoo --single-select-option-id string 282f80fd
+
+ # ---
+
+### Item Add: https://github.com/strangelove-ventures/interchaintest/issues/1094
+gh project item-add 34 --url https://github.com/strangelove-ventures/interchaintest/issues/1094 --owner strangelove-ventures
+
+###### Item Edit: PVTI_lADOBTYxNs4AkKTLzgQlDhI
+gh project item-edit --id PVTI_lADOBTYxNs4AkKTLzgQlDhI --project-id PVTI_lADOAsuzxc4AkKP5zgQ9Oxg --field-id PVTSSF_lADOAsuzxc4AkKP5zgcaQoo --single-select-option-id string 282f80fd
+
+ # ---
+
